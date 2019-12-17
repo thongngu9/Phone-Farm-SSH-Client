ce19e2ffefff198b8a4b5b1a781d2c62c1289071
thongngu9 <dangphuthong@gmail.com>
2019-12-18 05:40:32 +0700

    1.0.0

---
+++ Phone Farm SSH/Frm_PhoneFarmSSHClient.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Phone_Farm_SSH
+{
+    partial class Frm_PhoneFarmSSHClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_PhoneFarmSSHClient));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCon = new System.Windows.Forms.Button();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.White;
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnCon);
+            this.groupBox1.Controls.Add(this.btnImport);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtPort);
+            this.groupBox1.Location = new System.Drawing.Point(5, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(643, 49);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Server";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(362, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // btnCon
+            // 
+            this.btnCon.Location = new System.Drawing.Point(281, 15);
+            this.btnCon.Name = "btnCon";
+            this.btnCon.Size = new System.Drawing.Size(75, 23);
+            this.btnCon.TabIndex = 4;
+            this.btnCon.Text = "Connect";
+            this.btnCon.UseVisualStyleBackColor = true;
+            this.btnCon.Click += new System.EventHandler(this.btnCon_Click);
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(7, 15);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(150, 23);
+            this.btnImport.TabIndex = 2;
+            this.btnImport.Text = "Import";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(163, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Port";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(195, 17);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(80, 20);
+            this.txtPort.TabIndex = 3;
+            this.txtPort.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPort_KeyPress);
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToAddRows = false;
+            this.dgv.AllowUserToDeleteRows = false;
+            this.dgv.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(5, 67);
+            this.dgv.Name = "dgv";
+            this.dgv.ReadOnly = true;
+            this.dgv.Size = new System.Drawing.Size(643, 435);
+            this.dgv.TabIndex = 1;
+            this.dgv.Click += new System.EventHandler(this.dgv_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "Text file|*.txt";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.White;
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.btnDown);
+            this.groupBox2.Controls.Add(this.btnStop);
+            this.groupBox2.Controls.Add(this.btnRun);
+            this.groupBox2.Location = new System.Drawing.Point(654, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(211, 490);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Phone Farm";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 147);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "label4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            // 
+            // btnDown
+            // 
+            this.btnDown.Location = new System.Drawing.Point(6, 461);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(75, 23);
+            this.btnDown.TabIndex = 2;
+            this.btnDown.Text = "Down";
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(7, 75);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(198, 50);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(6, 19);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(198, 50);
+            this.btnRun.TabIndex = 0;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // Frm_PhoneFarmSSHClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(870, 514);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Frm_PhoneFarmSSHClient";
+            this.Text = "Phone Farm SSH Client";
+            this.Load += new System.EventHandler(this.Frm_PhoneFarmSSHClient_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button btnCon;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ Phone Farm SSH/Frm_PhoneFarmSSHClient.cs
@@ -0,0 +1,337 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Renci.SshNet;
+using System.Threading;
+
+namespace Phone_Farm_SSH
+{
+    public partial class Frm_PhoneFarmSSHClient : Form
+    {
+        Bitmap logoGoogle, numPage;
+        public Frm_PhoneFarmSSHClient()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        void LoadData()
+        {
+            logoGoogle = (Bitmap)Bitmap.FromFile("image//LogoGoogle.png");
+            numPage = (Bitmap)Bitmap.FromFile("image//NumPage.png");
+        }
+
+        DataTable table = new DataTable();
+        String[] filePaths;
+        private SshClient _client;
+        string Host, Username, Password;
+        bool isStop = false;
+        int index;
+        int numAds, notAds;
+
+        void Connect_SSH(int index)
+        {
+            Host = dgv.Rows[index].Cells["Host"].Value.ToString();
+            Username = dgv.Rows[index].Cells["Username"].Value.ToString();
+            Password = dgv.Rows[index].Cells["Password"].Value.ToString();
+            try
+            {
+                if (_client != null)
+                {
+                    _client.Disconnect();
+                    label2.ForeColor = Color.Black;
+                }
+                _client = new SshClient(Host, Username, Password);
+                _client.ConnectionInfo.Timeout = TimeSpan.FromSeconds(5);
+                _client.Connect();
+                ForwardedPortDynamic port = new ForwardedPortDynamic("127.0.0.1", Convert.ToUInt16(txtPort.Text));
+                _client.AddForwardedPort(port);
+                if (_client.IsConnected)
+                {
+                    port.Start();
+                    //label2.Visible = true;
+                    //label2.ForeColor = Color.Green;
+                    //label2.Text = "Connected Host: " + Host;
+                    dgv.Rows[index].DefaultCellStyle.BackColor = ColorTranslator.FromHtml("#abfeac");
+                }
+            }
+            catch (Renci.SshNet.Common.SshOperationTimeoutException)
+            {
+                _client.Disconnect();
+                dgv.Rows[index].DefaultCellStyle.BackColor = ColorTranslator.FromHtml("#fffcab");
+                NextRow();
+
+            }
+            catch (Renci.SshNet.Common.SshAuthenticationException)
+            {
+                _client.Disconnect();
+                dgv.Rows[index].DefaultCellStyle.BackColor = ColorTranslator.FromHtml("#ffabab");
+                NextRow();
+            }
+            catch (Renci.SshNet.Common.SshConnectionException)
+            {
+                _client.Disconnect();
+                dgv.Rows[index].DefaultCellStyle.BackColor = ColorTranslator.FromHtml("#ffabab");
+                NextRow();
+            }
+            catch (System.Net.Sockets.SocketException)
+            {
+                //_client.Disconnect();
+                //dgv.Rows[index].DefaultCellStyle.BackColor = ColorTranslator.FromHtml("#ffabab");
+                //NextRow();
+                MessageBox.Show("Port đã được sử dụng");
+            }
+            catch (Exception ex)
+            {
+                _client.Disconnect();
+                MessageBox.Show("Lỗi: "+ex.ToString(), "Lỗi phát sinh");
+            }
+        }
+
+        void Auto()
+        {
+            List<string> devices = new List<string>();
+            var listDevice = KAutoHelper.ADBHelper.GetDevices();
+            if (listDevice != null && listDevice.Count > 0)
+            {
+                devices = listDevice;
+            }
+
+            foreach (var deviceID in devices)
+            {
+                Task t = new Task(() =>
+                {
+                    numAds = 0; notAds = 0;
+                    while (true)
+                    {
+                        // Click vào trình duyệt
+                        if (isStop)
+                            return;
+                        KAutoHelper.ADBHelper.Tap(deviceID, 110, 625);
+                        Delay(5);
+
+                        // Mở menu
+                        if (isStop)
+                            return;
+                        KAutoHelper.ADBHelper.Tap(deviceID, 765, 120);
+                        Delay(1);
+
+                        // Chọn tab ẩn danh
+                        if (isStop)
+                            return;
+                        KAutoHelper.ADBHelper.Tap(deviceID, 591, 176);
+                        Delay(1);
+
+                        // Click vào ô địa chỉ
+                        if (isStop)
+                            return;
+                        //KAutoHelper.ADBHelper.Tap(deviceID, 369, 124);
+                        KAutoHelper.ADBHelper.Tap(deviceID, 455, 120);
+                        Delay(1);
+
+                        // Nhập địa chỉ
+                        if (isStop)
+                            return;
+                        KAutoHelper.ADBHelper.InputText(deviceID, "http://thongdang.herokuapp.com");
+                        Delay(1);
+
+                        // Nhấn Enter
+                        if (isStop)
+                            return;
+                        KAutoHelper.ADBHelper.Key(deviceID, KAutoHelper.ADBKeyEvent.KEYCODE_ENTER);
+                        Delay(25);
+
+                        // Kéo xuống
+                        Random rnd = new Random();
+                        int timesDown = rnd.Next(8, 12);
+                        for (int i = 1; i < timesDown; i++)
+                        {
+                            // Kéo 50% trang
+                            if (isStop)
+                                return;
+                            KAutoHelper.ADBHelper.Swipe(deviceID, 620, 1133, 748, 811);
+                            Delay(2);
+                        }
+
+                        // Kéo lên
+                        for (int j = 1; j < timesDown; j++)
+                        {
+                            // Kéo 100% trang
+                            if (isStop)
+                                return;
+                            KAutoHelper.ADBHelper.Swipe(deviceID, 748, 811, 620, 1133);
+                            Delay(2);
+                        }
+
+                        //if (isStop)
+                        //    return;
+                        //KAutoHelper.ADBHelper.Tap(deviceID, 381, 680);
+                        //Delay(2);
+
+                        //if (isStop)
+                        //    return;
+                        //KAutoHelper.ADBHelper.Tap(deviceID, 536, 1151);
+                        //Delay(10);
+
+                        // Thoát ứng dụng
+                        if (isStop)
+                            return;
+                        KAutoHelper.ADBHelper.Key(deviceID, KAutoHelper.ADBKeyEvent.KEYCODE_APP_SWITCH);
+                        Delay(1);
+
+                        // Kéo ngang để đóng hẳn ứng dụng
+                        if (isStop)
+                            return;
+                        KAutoHelper.ADBHelper.Swipe(deviceID, 144, 517, 672, 524);
+                        Delay(2);
+                    }
+                });
+                t.Start();
+            }
+        }
+
+        bool CheckImg(string deviceID, Bitmap image)
+        {
+            try
+            {
+                var screen = KAutoHelper.ADBHelper.ScreenShoot(deviceID);
+                var point = KAutoHelper.ImageScanOpenCV.FindOutPoint(screen, image);
+                if (point != null)
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        void Delay(int delay)
+        {
+            while (delay > 0)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+                delay--;
+                if (isStop)
+                    break;
+            }
+        }
+
+        void NextRow()
+        {
+            if (index < dgv.RowCount - 1)
+            {
+                dgv.ClearSelection();
+                //dgv.Rows[++index].Selected = true;
+                index = ++index;
+                Connect_SSH(index);
+            }
+        }
+
+        private void Frm_PhoneFarmSSHClient_Load(object sender, EventArgs e)
+        {
+            label2.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+            btnDown.Visible = false;
+            btnCon.Enabled = false;
+            groupBox2.Enabled = false;
+            table.Columns.Add("Host", typeof(string));
+            table.Columns.Add("Username", typeof(string));
+            table.Columns.Add("Password", typeof(string));
+            table.Columns.Add("Country", typeof(string));
+            table.Columns.Add("Region", typeof(string));
+            table.Columns.Add("City", typeof(string));
+            table.Columns.Add("Zip code", typeof(string));
+            dgv.DataSource = table;
+            index = dgv.CurrentCell.RowIndex;
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                btnCon.Enabled = true;
+                filePaths = openFileDialog.FileNames;
+                foreach (string fileUrl in filePaths)
+                {
+                    string[] lines = File.ReadAllLines(fileUrl);
+                    string[] values;
+                    for (int i = 0; i < lines.Length; i++)
+                    {
+                        values = lines[i].ToString().Split('|');
+                        string[] row = new string[values.Length];
+                        for (int j = 0; j < values.Length; j++)
+                        {
+                            row[j] = values[j].Trim();
+                        }
+                        table.Rows.Add(row);
+                    }
+                }
+            }
+        }
+
+        private void txtPort_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            Task t = new Task(() =>
+            {
+                isStop = false;
+                Auto();
+            });
+            t.Start();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            isStop = true;
+        }
+
+        private void btnDown_Click(object sender, EventArgs e)
+        {
+            NextRow();
+        }
+
+        private void dgv_Click(object sender, EventArgs e)
+        {
+            if (txtPort.Text == "")
+            {
+                MessageBox.Show("Port không hợp lệ, vui lòng kiểm tra lại.");
+            }
+            else
+            {
+                index = dgv.CurrentCell.RowIndex;
+                Connect_SSH(index);
+            }
+        }
+
+        private void btnCon_Click(object sender, EventArgs e)
+        {
+            if (txtPort.Text == "")
+            {
+                MessageBox.Show("Port không hợp lệ, vui lòng kiểm tra lại.");
+            }
+            else
+            {
+                Connect_SSH(0);
+                groupBox2.Enabled = true;
+            }
+        }
+    }
+}
